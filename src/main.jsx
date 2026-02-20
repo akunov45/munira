1d22e859aa7f99bd87eaac1261d9e672192d4ef9
Akunov4594 <86850933+akunov45@users.noreply.github.com>
2026-02-20 12:51:20 +0600

    return  old name

--- src/main.jsx
+++ src/main.jsx
@@ -2,26 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import "./config/i18n.js"
-import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
-import { StyledEngineProvider } from '@mui/material/styles';
+import {Provider} from "react-redux";
+import {store} from "./redux/store.js";
+import {StyledEngineProvider} from '@mui/material/styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter
-    future={{
-      v7_relativeSplatPath: true,
-      v7_startTransition: true
-    }}
-    basename='/aqvela/'
-  >
-    <Provider store={store}>
-      <React.StrictMode>
-        <StyledEngineProvider injectFirst>
-          <App />
-        </StyledEngineProvider>
-      </React.StrictMode>
-    </Provider>
-  </BrowserRouter>
+	<BrowserRouter
+		future={{
+			v7_relativeSplatPath: true,
+			v7_startTransition: true
+		}}
+    basename='/munira'
+    >
+		<Provider store={store}>
+			<React.StrictMode>
+				<StyledEngineProvider injectFirst>
+					<App/>
+				</StyledEngineProvider>
+			</React.StrictMode>
+		</Provider>
+	</BrowserRouter>
 )
