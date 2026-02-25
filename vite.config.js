43876f2a7365fbfceb6212c3b62c12960d160eec
Akunov4594 <86850933+akunov45@users.noreply.github.com>
2026-02-25 23:11:02 +0600

    base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 import removeConsole from 'vite-plugin-remove-console';
 
 export default defineConfig({
-  plugins: [react(), removeConsole()],
+  plugins: [react(), removeConsole()
+  ],
+  base: '/munira/'
 })
